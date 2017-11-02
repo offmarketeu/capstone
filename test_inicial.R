70797be8b6918189b8aad64d7c31e1c005f11a8e
offmarketeu <analisis@offmarket.eu>
2017-11-02 22:47:13 +0100

    Add files via upload

--- test_inicial.R
+++ test_inicial.R
@@ -1,8 +1,12 @@
 library(quanteda)
 
-dir<-"E:/COURSERA/Data Science/Course10-Capstone/"
-dir1<-"E:/COURSERA/Data Science/Course10-Capstone/CORP"
-dir2<-"E:/COURSERA/Data Science/Course10-Capstone/SAMPLE"
+#dir<-"E:/COURSERA/Data Science/Course10-Capstone/"
+#dir1<-"E:/COURSERA/Data Science/Course10-Capstone/CORP"
+#dir2<-"E:/COURSERA/Data Science/Course10-Capstone/SAMPLE"
+
+dir<-"C:/Users/n040485/Documents/000_CAPSTONE/"
+dir1<-"C:/Users/n040485/Documents/000_CAPSTONE/CORP"
+dir2<-"C:/Users/n040485/Documents/000_CAPSTONE/SAMPLE"
 
 #fullCorpus <- Corpus(DirSource(dir2), readerControl = list(language="en_US"))
 
@@ -20,4 +24,6 @@ cor<- corptc+corptc2
 tkdfm <- dfm(tct, tolower=TRUE, remove=c(profanity, stopwords("spanish")), remove_numbers = TRUE, remove_punct=TRUE, ngrams = 1, verbose = FALSE)
 tkdfm2 <- dfm(tct, tolower=TRUE, remove=c(profanity, stopwords("spanish")), remove_numbers = TRUE, remove_punct=TRUE,stem = TRUE, ngrams = 2, verbose = FALSE)
 presidentes <- dfm(tct, tolower=TRUE, select="president", ngrams = 2, verbose = FALSE)
+presi<- kwic(cor, phrase("president"))
+presi
 
